5e46f59a81a856e58af06338ff1baa1fa63a42a7
ssongman <ssongmantop@gamil.com>
2018-09-24 21:27:27 +0900

    first commit

---
+++ UserList/src/main/java/com/kshrd/springbootdemo/model/User.java
@@ -0,0 +1,69 @@
+package com.kshrd.springbootdemo.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class User {
+	
+	private int id;
+
+	@NotEmpty(message = "User name cannot be empty!")
+	@Size(min = 2, max = 30, message = "User name must be between {2} and {1} characters!")
+	private String name;
+
+	@Size(min = 1, max = 1, message = "Gender must be one character long!")
+	private String gender;
+
+	//@NotEmpty(message = "Image can't be empty!")
+	private String image;
+
+	public User() {
+	}
+
+	public User(int id, String name, String gender, String image) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.image = image;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", gender=" + gender + ", image=" + image + "]";
+	}
+
+}
