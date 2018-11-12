a7b8d6f222a07016f3172047ecbb08f99bd375c7
ssongman <ssongmantop@gamil.com>
2018-11-12 22:04:24 +0900

    REST API again

--- spring-boot-demo/src/main/java/com/kshrd/springbootdemo/controller/UserController.java
+++ spring-boot-demo/src/main/java/com/kshrd/springbootdemo/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kshrd.springbootdemo.model.User;
